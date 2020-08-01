b9ee0a56204f05816040e0663af07d03779d7764
Lmagicport <2034913866@qq.com>
2020-08-02 00:39:25 +0800

    test

--- Get_Data.py
+++ Get_Data.py
@@ -15,11 +15,12 @@ class SpySharesData:
         self.flag = f
     
     # 数据来自上海交易所和深圳交易所官方网站，如果没有找到需要的股票代码，可以自行对代码进行补充，补充后的文件欢迎上传至项目中
-    def spydatalist(self,shares_code):
+    def spydatalist(self, shares_code):
         # flag=1 表示股票数据来自上海股票交易所中的A股列表
         if self.flag == 1:
             data_xlsx = pd.read_excel(r'shares_data\sh_a.xlsx')
             shares_data = pd.DataFrame(data_xlsx)
+
 
             
             
