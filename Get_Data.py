bce65fe21abc01279898774a9273332fbed58717
Lmagicport <2034913866@qq.com>
2020-08-01 22:22:12 +0800

    add some data

--- Get_Data.py
+++ Get_Data.py
@@ -4,17 +4,25 @@ in this moudle you can get shares’s data from internet
 
 # get shares list from money.163
 
-from requests_html import HTMLSession
+import numpy as np
+import pandas as pd
 
 
 class SpySharesData:
 
-    def __init__(self,f):
+    def __init__(self, f=0):
         'f means which market you need to get data'
         self.flag = f
     
-    # spy share's list from ifeng.com
-    def spydatalist(self):
+    # 数据来自上海交易所和深圳交易所官方网站，如果没有找到需要的股票代码，可以自行对代码进行补充，补充后的文件欢迎上传至项目中
+    def spydatalist(self,shares_code):
+        # flag=1 表示股票数据来自上海股票交易所中的A股列表
         if self.flag == 1:
-            session = HTMLSession()
-            r = session.get('')
+            data_xlsx = pd.read_excel(r'shares_data\sh_a.xlsx')
+            
+            
+            
+            
+
+            
+            
