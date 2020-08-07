ef0a73ac2a2bfc2446ee7b99e78e9b0bd4f6edc1
Lmagicport <2034913866@qq.com>
2020-08-08 01:45:40 +0800

    complte a function

--- Get_Data.py
+++ Get_Data.py
@@ -14,6 +14,7 @@ class SpySharesData:
     def __init__(self, f=0):
         'f means which market you need to get data'
         self.flag = f
+        self.SharesFilePath = None
     
     # 数据来自上海交易所和深圳交易所官方网站，如果没有找到需要的股票代码，可以自行对代码进行补充，补充后的文件欢迎上传至项目中
     def spydatalist(self, shares_code) -> pd.DataFrame:
@@ -30,7 +31,7 @@ class SpySharesData:
                     return self.CompanyInfo
 
             except Exception as e:
-                print(e)
+                return e
 
         elif self.flag == 2:
             # flag =2 表示股票数据来自上海股票交易所中的B股列表
@@ -43,21 +44,30 @@ class SpySharesData:
                     self.CompanyInfo = pd.concat([self.CompanyCode, self.CompanyName],axis = 1)
                     return self.CompanyInfo
             except Exception as e:
-                print(e)
+                return e
         
         elif self.flag == 3:
             # flag = 3 表示 股票数据来自深圳股票交易所中的A股列表
             try:
                 # 读取所需信息
-                data_csv = pd.DataFrame(pd.read_csv(r'shares_data\sz_a', converters={u'A股简称':str,u'A股代码':str}))
+                data_csv = pd.DataFrame(pd.read_csv(r'shares_data\sz_a.csv', converters={u'A股简称':str,u'A股代码':str}))
                 self.CompanyName = data_csv.iloc[:,3]
                 self.CompanyCode = data_csv.iloc[:,4]
                 self.CompanyInfo = pd.concat([self.CompanyCode, self.CompanyName], axis=1)
                 return self.CompanyInfo
             except Exception as e:
-                print(e)
+                return e
         
         elif self.flag == 4:
             # flag = 4 表示股票数据来自于深圳交易所中的B股列表
-
-                    
+            try:
+                # 读取所需信息
+                data_csv = pd.DataFrame(pd.read_csv(r'shares_data\sz_b.csv', converters={u'B股代码':str,u'B股简称':str}))
+                self.CompanyName = data_csv.iloc[:, 7]
+                self.CompanyCode = data_csv.iloc[:, 6]
+                self.CompanyInfo = pd.concat([self.CompanyCode, self.CompanyName],axis=1)
+                return self.CompanyInfo
+            except Exception as e:
+                return e    
+    
+    
